8873630b0f405bbb0ffa21301b65e0a5bd618828
SnottyJACK <snottyjack@yandex.ru>
2020-08-10 23:20:59 +0500

    function for displaying vacancies

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from .models import Vacancy
 def home_view(request):
     qs = Vacancy.objects.all()
-    return render(request, 'home.html', {'object_list': qs})
+    return render(request, 'scraping\home.html', {'object_list': qs})
